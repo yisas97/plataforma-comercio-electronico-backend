5119c348b32be28e7b8c89f30c5ffada08fd0c05
Jesus Junior <43008669+yisas97@users.noreply.github.com>
2025-06-28 09:30:14 -0500

    Update CorsConfig.java

--- src/main/java/pe/com/prueba/plataformacontrolcomercio/config/CorsConfig.java
+++ src/main/java/pe/com/prueba/plataformacontrolcomercio/config/CorsConfig.java
@@ -11,7 +11,7 @@ public class CorsConfig implements WebMvcConfigurer
     public void addCorsMappings(CorsRegistry registry)
     {
         registry.addMapping("/**").allowedOrigins(
-                        "http://localhost:4200") // Puerto por defecto de Astro
+                        "*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*").allowCredentials(true);
     }
