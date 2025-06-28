bd412d5aad3489f8b01cd81646f810a049d38527
Jesus Junior <43008669+yisas97@users.noreply.github.com>
2025-06-28 10:05:54 -0500

    Update CorsConfig.java

--- src/main/java/pe/com/prueba/plataformacontrolcomercio/config/CorsConfig.java
+++ src/main/java/pe/com/prueba/plataformacontrolcomercio/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer
     @Override
     public void addCorsMappings(CorsRegistry registry)
     {
-        registry.addMapping("/**").allowedOrigins(
+        registry.addMapping("/**").allowedOriginPatterns(
                         "*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*").allowCredentials(true);
